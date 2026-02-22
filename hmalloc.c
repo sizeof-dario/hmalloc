e0fbcfb60f06c4086a6f98d0a4c11be5d3fb88be
sizeof-dario <dimeo.dario.h@gmail.com>
2026-02-22 15:47:33 +0100

    Added block splitting and fixed free()

--- hmalloc.c
+++ hmalloc.c
@@ -1,80 +1,96 @@
 #include "hmalloc.h"
 
-// Static variable keeping track of the heap starting address.
-// A value of `NULL` indicates that the heap starting address is at its lowest.
-static void *hstart = NULL;
+
+
+// Keeps track of the heap starting address.
+static void *heap_start = NULL;
+
+
 
 void *hmalloc(size_t size)
 {
-    // `hmalloc()` does slightly different things depending on whether the
-    // program break currently coincides with the heap start or not, so we use
-    // `firstcall` to keep track of that. By default, we assume the heap start
-    // and the program break do not coincide, assigning to `firstcall` a value
-    // of `0`.
-    //
-    // Note that the name `firstcall` doesn't mean the variable gets set to `1`
-    // only if `hmalloc()` is getting called for the first time. It only hints
-    // to the fact that's a trivial case in which the value does become `1`. 
-    int firstcall = 0;
+    // Memory alignment must be guaranteed for a well defined behaviour
+    // according to the C standard. Thus, we round up both `size` and the
+    // header size to the closest multiple of the value that satisfies
+    // alignment requirement of each standard type.
 
-    // We must guarantee alignment for a well defined behaviour according to
-    // the C standard. Thus, we round up both the `size` argument value and the
-    // memory block header size to the closest multiple of the alignment
-    // required by the greatest standard type with no alignment specifiers.
+    // Aligned `size`.
+    size_t al_payload_sz = ALIGN(size);
 
-    // Aligned `size` argument.
-    size_t al_blocksize = ALIGN(size);
-    // Aligned memory block header size.
-    size_t al_headersize = ALIGN(sizeof(mbheader));
-
-    // `hamlloc()` retrieves the virtual address of the heap start when called
-    // while `hstart` is `NULL`, and updates the variable value, along with
-    // `firstcall`.
-    //
-    // Note that calling `sbrk(0)` to update `hstart` is really only needed
-    // upon the first `hmalloc()` call. However, code simplicity is here
-    // preferred over perfect optimization.
-    if(hstart == NULL)
+    // We retrieve the heap start address upon first call. Once `heap_start` is
+    // set to a non-`NULL` value, this branch is never supposed to be extecuted
+    // again.
+    if(heap_start == NULL)
     {
-        hstart = sbrk(0);
-        firstcall = 1;
+        heap_start = sbrk(0);
+        //           ^^^^^^^ We suppose this call not to fail.
     }
 
-    // `hmalloc()` first scans the heap for a block of memory that is big
-    // enough to contain `al_blocksize` bytes of data. It will eventually move
-    // the program break up if it finds none.
+    // The first strategy used to allocate the requested memory is scanning the
+    // heap for a block whose payload is big enough to contain `al_payload_sz`
+    // bytes of data.
 
-    // Used to traverse the headers list.
-    void *hcurr = hstart;
+    // Program break address (end of the heap).
+    void *prog_brk = sbrk(0);
+    //               ^^^^^^^ We suppose this call not to fail.
+ 
+    // Used to traverse the blocks list.
+    mbheader *hdr_curr = (mbheader *)heap_start;
     // Used to link memory block headers.
-    void *hlast = NULL;
+    mbheader *hdr_last = NULL;
 
-    // Searching for a big enough block.
-    while(hcurr != NULL && !firstcall)
+    while(hdr_curr != NULL && (prog_brk != heap_start))
     {
-        // If we find a big enough free block, we mark it as occupied and
-        // return the block address.
-        if(((mbheader *)hcurr)->free 
-            && ((mbheader *)hcurr)->blocksize >= al_blocksize)
+        // If we find a big enough free block, we check if block splitting is
+        // possible and then return its payload address.
+        if(hdr_curr->free == 1 && (hdr_curr->payload_sz >= al_payload_sz))
         {
-            ((mbheader *)hcurr)->free = 0;
-            return ((char *)hcurr + al_headersize);
-        }
+            hdr_curr->free = 0;
+            // We can perform block splitting only if what would remeain of the
+            // block we found is big enough for the minimum allocatable block.
+            if((hdr_curr->payload_sz - al_payload_sz) >= MIN_BLOCK_SZ)
+            {
+                size_t old_payload_sz = hdr_curr->payload_sz;
+                mbheader *old_hdr_next = hdr_curr->hdr_next;
 
-        // We save the current header pointer in case we exit the loop, meaning
-        // it was associated with the last memory block. In this way, we can
-        // access it and link it to the soon newly added block via `sbrk()`.
-        hlast = hcurr;
-        
-        hcurr = ((mbheader *)hcurr)->next;
+                mbheader *hdr_new = (mbheader *)
+                    ((char *)hdr_curr + AL_HDR_SZ + al_payload_sz);
+
+                // Updating the current block.
+                hdr_curr->payload_sz = al_payload_sz;
+                hdr_curr->hdr_next = hdr_new;
+
+                // Initializing the new block.
+                hdr_new->payload_sz 
+                    = old_payload_sz - al_payload_sz - AL_HDR_SZ;
+                hdr_new->free = 1;
+                hdr_new->hdr_prev = hdr_curr;
+                hdr_new->hdr_next = old_hdr_next;
+
+                // Updating the next-in-the-heap block.
+                if(old_hdr_next != NULL)
+                {
+                    old_hdr_next->hdr_prev = hdr_new;
+                }
+            }
+
+            return ((char *)hdr_curr + AL_HDR_SZ);
+        }
+     
+        // We save the current header pointer in case we exit the loop before
+        // the next iteration, meaning the header was associated with the last 
+        // memory block. In this way, we can reaccess it and link it to the 
+        // soon newly added block via `sbrk()`.    
+        hdr_last = hdr_curr;
+
+        hdr_curr = hdr_curr->hdr_next;
     }
 
-    // Following code is executed if we failed to find a suitable block to our
-    // needs. In particular, `sbrk()` raises the program break and the returned
-    // pointer is used to calculate the block address to return.
+    // The second strategy used to allocate the requested memory is calling 
+    // `sbrk()` for raisung the program break.
 
     // Pointer to the header of the newly allocated block.
-    void *p = sbrk(al_blocksize + al_headersize);
+    void *p = sbrk((intptr_t)(al_payload_sz + AL_HDR_SZ));
     
     // There's a possibility `sbrk()` fails.
     if(p == (void *)-1)
@@ -82,91 +98,97 @@ void *hmalloc(size_t size)
         return NULL;
     }
 
-    // If `sbrk()` didn't fail, we must initialize the block header fields.
+    // If `sbrk()` didn't fail, we must initialize the header fields.
+    // Note that `((mbheader *)p)->hdr_prev = hdr_last;` also covers the case
+    // where the block is first in the heap because in that case `hdr_last` is 
+    // never changed from `NULL`.
     ((mbheader *)p)->free = 0;
-    ((mbheader *)p)->blocksize = al_blocksize;
-    ((mbheader *)p)->next = NULL;
+    ((mbheader *)p)->payload_sz = al_payload_sz;
+    ((mbheader *)p)->hdr_prev = hdr_last;
+    ((mbheader *)p)->hdr_next = NULL;
 
-    // Also, if the block allocated is not the first in the heap, we need to
-    // link the previous memory block to it.
-    if(!firstcall)
+    // Also, if the newly block allocated is not the first in the heap, we need
+    // to link the previous one to it.
+    if(prog_brk != heap_start)
     {
-        ((mbheader *)hlast)->next = p;
+        ((mbheader *)p)->hdr_prev->hdr_next = p;
     }
 
-    return (char *)p + al_headersize;
+    // We must return a pointer to the payload, not the header.
+    return ((char *)p + AL_HDR_SZ);
 }
+
+
 
 void hfree(void *p)
 {
-    // Aligned memory block header size.
-    size_t al_headersize = ALIGN(sizeof(mbheader));
-
-    // If `hfree()` receives a `NULL` pointer, the expected behaviour is to do
-    // nothing.
-    if(p != NULL)
+    // If argument is `NULL`, the expected behaviour is to do nothing.
+    if(p == NULL)
     {
-        // hfree() first checks if `p` is a valid pointer by comparing it, with
-        // the right offset, to every possible valid header pointer. If it
-        // fails to do so, it does nothing.
-        
-        // Pointer to the header of the block pointed by `p`.
-        mbheader *p_header_ptr = (mbheader *)((char *)p - al_headersize);
+        return;
+    }
 
-        // Pointer used to traverse the header list.
-        mbheader *curr_header_ptr = (mbheader *)hstart;
+    // We must check if `p` is a valid pointer by comparing its header to every
+    //  possible valid header pointer. Finding no match means the pointer is
+    // invalid, it that case we do nothing.
 
-        // Pointer used to possibly update `next` in the second-to-last block
-        // in the heap if the last block gets freed.
-        mbheader *prev_header_ptr = NULL;
+    // Pointer to the header of `p`.
+    mbheader *p_hdr = (mbheader *)((char *)p - AL_HDR_SZ);
 
-        // We traverse the headers list looking for the header associated to
-        // `p`. Not finding it would imply that the pointer is invalid.
-        while(curr_header_ptr != p_header_ptr)
+    // Used to traverse the header list.
+    mbheader *hdr_curr = (mbheader *)heap_start;
+
+    while(hdr_curr != p_hdr)
+    {
+        hdr_curr = hdr_curr->hdr_next;
+        if(hdr_curr == NULL)
         {
-            prev_header_ptr = curr_header_ptr;
-            curr_header_ptr = curr_header_ptr->next;
-            if(curr_header_ptr == NULL)
-            {
-                // It means we reached the end of the heap without finding a
-                // valid match.
-                return;
-            }
-        }
-
-        // `hfree()` must also make sure that `p` doesn't point to an already
-        // freed block for it to be considered valid.
-        if(p_header_ptr->free)
-        {
+            // We reached the end of the heap with no valid match.
             return;
         }
-
-        // We can now assume `p` is valid.
-
-        // If `hfree()` receives a pointer to the last block in the heap, it
-        // decreases the program break and sets to `NULL` the `next` pointer of
-        // the new last header. Otherwise, it just marks the block as free for 
-        // future reusage.
-        if(((mbheader *)p_header_ptr)->next == NULL)
-        {
-            // This call to `sbrk()` will assumed not to fail.
-            sbrk(-(p_header_ptr->blocksize + al_headersize));
-
-            // We have to make sure we are not freeing the first block of the
-            // heap before trying to access `prev_header_ptr`. Also, if we are
-            // freeing such block, we reset `hstart` back to `NULL`.
-            if(prev_header_ptr != NULL)
-            {
-                prev_header_ptr->next = NULL;
-            }
-            else
-            {
-                hstart = NULL;
-            }
-        }
-        else
-        {
-            p_header_ptr->free = 1;
-        }      
     }
+
+    // We must also check if we are trying to double-free a block.
+    if(p_hdr->free)
+    {
+        return;
+    }
+
+    // We can now assume `p` is valid.
+
+    // If `p` points to the last block in the heap, we can lower the program
+    // break. Otherwise, we just mark the block as free for future reusage.
+    if(((mbheader *)p_hdr)->hdr_next == NULL)
+    {
+        // We reuse `hdr_curr` to possibly traverse back the heap if multiple
+        // free blocks are present, to lower the program break.
+        hdr_curr = ((mbheader *)p_hdr);
+        // Keeps track of how much the program break will decrease.
+        size_t shift = -(hdr_curr->payload_sz +  AL_HDR_SZ);
+
+        while(hdr_curr->hdr_prev != NULL && hdr_curr->hdr_prev->free == 1)
+        {
+            // As long as we don't reach the start of the heap, we increase (as
+            // a negative value) `shift` and set to `NULL` the `hdr_next` 
+            // pointer of the previous header. 
+            shift -= (hdr_curr->hdr_prev->payload_sz +  AL_HDR_SZ);
+            hdr_curr->hdr_prev->hdr_next = NULL;
+
+            hdr_curr = hdr_curr->hdr_prev;
+        }
+
+        // Call assumed not to fail.
+        sbrk((intptr_t)(shift));
+               
+        // If we reached the start of the heap, we reset `heap_start` back to
+        // `NULL`.
+        if(hdr_curr->hdr_prev == NULL)
+        {
+            heap_start = NULL;
+        }
+    }
+    else
+    {
+        p_hdr->free = 1;
+    }      
 }
--- hmalloc.h
+++ hmalloc.h
@@ -1,30 +1,55 @@
 #ifndef HMALLOC_H
 #define HMALLOC_H 1
 
+
+
 // Feature test macro required for `sbrk()` since glibc 2.19.
 #define _DEFAULT_SOURCE
+
+
 
 #include <stdalign.h>   // For `alignof()`.
 #include <stddef.h>     // For `max_align_t`.
 #include <unistd.h>     // For `sbrk()`.
 
+
+
 // Memory block header used by `hmalloc()` and `hfree()`.
 typedef struct mbheader
 {
-    size_t blocksize;       // Size of the block associated to this.
-    int free;               // Flag for whether a block is free or occupied.
-    struct mbheader *next;  // Pointer to the next memory block header.
+    size_t payload_sz;          // Size of the block payload.
+    int free;                   // Tells whether a block is free or occupied.
+    struct mbheader *hdr_prev;  // Pointer to the previous memory block header.
+    struct mbheader *hdr_next;  // Pointer to the next memory block header.
 } mbheader;
 
-// Aligns `x` to the alignment of at least the greatest standard type with no
-// alignment specifiers.
-#define ALIGN(x) \
-    (((x) + alignof(max_align_t) - 1) & ~(alignof(max_align_t) - 1))
+
+
+// Aligns `size` to the alignment of at least the greatest standard type with
+// no alignment specifiers.
+#define ALIGN(size) \
+    (((size) + alignof(max_align_t) - 1) & ~(alignof(max_align_t) - 1))
+
+
+
+// Aligned header size
+#define AL_HDR_SZ ALIGN(sizeof(mbheader))
+
+
+
+// Minimum size for a block.
+#define MIN_BLOCK_SZ (AL_HDR_SZ + ALIGN(sizeof(char)))
+
+
 
 // Allocates `size` bytes of heap memory.
 void *hmalloc(size_t size);
 
+
+
 // Frees heap memory pointed by `p` and allocated by `hmalloc()`.
 void hfree(void *p);
+
+
 
 #endif // HMALLOC_H
